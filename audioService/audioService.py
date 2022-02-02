b40547480d55840c9e25b5bdc414dddde25cbe73
evelin <evelin>
2022-02-02 18:42:16 +0200

    Ros demo with audio

---
+++ audioService/audioService.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+from gtts import gTTS
+from playsound import playsound
+import os
+
+def callback(data):
+	rospy.loginfo('Msg received: "%s" ', data.data)
+	# Language in which you want to convert
+	language = 'el'
+
+	# Passing the text and language to the engine,
+	# here we have marked slow=False. Which tells
+	# the module that the converted audio should
+	# have a high speed
+	myobj = gTTS(text=data.data, lang=language, slow=False)
+
+	# Saving the converted audio in a mp3 file named
+	# welcome
+	myobj.save("audio.mp3")
+
+	# Playing the converted file
+	#os.system("mpg321 welcome.mp3")
+	# for playing note.wav file
+	playsound('audio.mp3')
+	os.remove('audio.mp3')
+	
+
+def listener():
+
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('chatter', String, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
