ae0bd9c62df758e338969f1b914e7dab1537fbc4
evelin <evelin>
2022-02-05 19:05:20 +0200

    change names

---
+++ userInterface/mainMenu.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from model.mainMenu import MainModel
+from controllers.menuCntl import MenuCntl
+from views.menuView import MenuView
+
+
+class App(QApplication):
+    def __init__(self, sys_argv):
+        super(App, self).__init__(sys_argv)
+        self.model = MainModel()
+        self.main_controller = MenuCntl(self.model)
+        self.main_view = MenuView(self.model, self.main_controller)
+        self.main_view.show()
+
+
+if __name__ == '__main__':
+    app = App(sys.argv)
+    print('hello!!')
+    sys.exit(app.exec_())
