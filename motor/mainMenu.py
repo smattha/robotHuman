fb38a2a98630b530194d744b6a39c0df46b19fdd
Stergios Matthaiakis <stergiosmat@gmail.com>
2022-10-16 10:38:53 +0300

    Before thread git status

--- motor/mainMenu.py
+++ motor/mainMenu.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from pyexpat import model
+
 import sys
+from xmlrpc.client import boolean
 from PyQt5.QtWidgets import QApplication
 from views.menuView import MenuView
 from rosInterface.audioServiceInterface import Ros_Audio_Service
@@ -13,73 +14,8 @@ class App(QApplication):
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
 
-        # self.path="/home/stergios/git/src/robotHuman/user_interface"
-
-        # if (len(sys.argv)>=2):
-        #     self.path =  sys.argv[1]
-        #     self.i=int( sys.argv[2])
-        #     self.displaymageRation=int(sys.argv[3])
-        #     self.flag=sys.argv[4]
-        #     print("...................")
-        # else:
-        #     self.path = "/home/stergios/git/src/robotHuman/user_interface"
-        #     self.i=2
-        #     self.displaymageRation=1
-        #     self.flag="test"
-
-        # self.model= MainWindowModel( self.path)
-
-        # self.model.i= self.i
-        # self.model.displayImageRatio =self.displaymageRation
-
-
-        # self._rosInterface=Ros_Audio_Service()
-        # self._rosInterface.flag=self.flag
+        motor=MoveController(True);
         
-
-        # self._controllerEx1= ControllerType1(self._rosInterface,self.model)
-        # self._controllerEx1.setVariable1()
-
-        # self._controllerEx2= ControllerType1(self._rosInterface,self.model)
-        # self._controllerEx2.setVariable2()
-
-        # self._controllerEx3= ControllerType2(self._rosInterface,self.model)
-        # self._controllerEx3.setVariables3()
-
-        # self._controllerEx4= ControllerType2(self._rosInterface,self.model)
-        # self._controllerEx4.setVariables4()
-
-        # self._controllerEx5= ControllerExersice5(self._rosInterface,self.model)
-        # self._controllerEx5.setVariable1()
-
-        # self._controllerEx6= ControllerExersice6(self._rosInterface,self.model)
-        # self._controllerEx6.setVariable6()
-
-
-
-
-        # self._controllerEx7= ControllerType1(self._rosInterface,self.model)
-        # self._controllerEx7.setVariableB1()
-
-        # self._controllerEx8= ControllerType1(self._rosInterface,self.model)
-        # self._controllerEx8.setVariableB2()
-
-        # self._controllerEx9= ControllerType2(self._rosInterface,self.model)
-        # self._controllerEx9.setVariablesB3()
-
-        # self._controllerEx10= ControllerType2(self._rosInterface,self.model)
-        # self._controllerEx10.setVariablesB4()
-
-        # self._controllerEx11= ControllerExersice5(self._rosInterface,self.model)
-        # self._controllerEx11.setVariable2()
-
-        # self._controllerEx12= ControllerExersice6(self._rosInterface,self.model)
-        # self._controllerEx12.setVariable6B()
-
-        # self.main_controller = MenuCntl(self.model,[self._controllerEx1,self._controllerEx2,self._controllerEx3,self._controllerEx4,self._controllerEx5,self._controllerEx6,
-        #                                            self._controllerEx7,self._controllerEx8,self._controllerEx9,self._controllerEx10,self._controllerEx11,self._controllerEx12])
-        motor=MoveController();
-        motor.getPosition(int(1))
         self.menu_view =MenuView(motor)
         self.menu_view.show()
 
--- motor/motorController/MotorController.py
+++ motor/motorController/MotorController.py
@@ -1,13 +1,12 @@
-
-from pickle import TRUE
-from tkinter import FALSE
 from motorController.Ax12 import Ax12
 
 class MoveController():
-        def __init__(self):
+        def __init__(self,offline):
+
+            print ( 'Initialize move controller in offline '+str(True))
             super().__init__()
-            self.offline=FALSE
-            self.pos=0;
+            self.offline=offline
+            self.counterForRandomPos=0;
 
             self.leftHand=0
             self.rightHand=1
@@ -18,7 +17,7 @@ class MoveController():
 
             self.speed=85
 
-            if self.offline== FALSE:
+            if self.offline== False:
             
                 # e.g 'COM3' windows or '/dev/ttyUSB0' for Linux
                 #Ax12.DEVICENAME = '/dev/ttyACM1'
@@ -30,9 +29,9 @@ class MoveController():
 
 
         def getPosition(self,motor_id):
-            if self.offline==TRUE:
-                self.pos=self.pos+1
-                return 10000*motor_id+self.pos
+            if self.offline==True:
+                self.counterForRandomPos=self.counterForRandomPos+1
+                return 10000*motor_id+self.counterForRandomPos
             my_dxl2 = Ax12(motor_id)
             my_dxl2.set_moving_speed(self.speed)
             my_dxl2.set_led(0)
@@ -46,75 +45,19 @@ class MoveController():
         
         def move(self,motor_id,input_pos):
                 print("Move motor" , str(motor_id) , " by  " ,str(input_pos))
-                if self.offline==FALSE:
+                if self.offline==False:
                     my_dxl2 = Ax12(motor_id)
                     my_dxl2.set_goal_position(my_dxl2.get_present_position()+input_pos)
                     my_dxl2.set_moving_speed(85)
                     
         def moveAbs(self,input_pos,motor_id):
                 print("Move motor" , str(motor_id) , " to pos  " ,str(input_pos))
-                if self.offline==FALSE:
+                if self.offline==False:
                     my_dxl2 = Ax12(motor_id)
                     my_dxl2.set_goal_position(input_pos)
                     my_dxl2.set_moving_speed(85)
 
-# # create AX12 instance with ID 10
-# motor_id = 0
-# my_dxl = Ax12(motor_id)
-# my_dxl.set_moving_speed(5)
-# my_dxl.set_led(0)
-# # my_dxl.set_goal_position(400)
-
-
-# motor_id = 1
-# my_dxl2 = Ax12(motor_id)
-# my_dxl2.set_moving_speed(5)
-# my_dxl2.set_led(0)
 
 
 
 
-# print("\nPosition of dxl ID: %d is %d " %
-#               (my_dxl.id, my_dxl.get_present_position()))
-
-# print("\nPosition of dxl ID: %d is %d " %
-#               (my_dxl2.id, my_dxl2.get_present_position()))
-
-# my_dxl.set_goal_position(100)
-
-# my_dxl2.set_goal_position(450)
-
-
-
-
-
-# def user_input():
-#     """Check to see if user wants to continue"""
-#     ans = input('Continue? : y/n ')
-#     if ans == 'n':
-#         return False
-#     else:
-#         return True
-
-
-# def main(motor_object):
-#     """ sets goal position based on user input """
-#     bool_test = True
-#     while bool_test:
-
-#         print("\nPosition of dxl ID: %d is %d " %
-#               (motor_object.id, motor_object.get_present_position()))
-#         # desired angle input
-#         input_pos = int(input("goal pos: "))
-#         motor_object.set_goal_position(input_pos)
-#         print("Position of dxl ID: %d is now: %d " %
-#               (motor_object.id, motor_object.get_present_position()))
-#         bool_test = user_input()
-
-
-# # pass in AX112 object
-# main(my_dxl)
-
-# # disconnect
-# my_dxl.set_torque_enable(0)
-# Ax12.disconnect()
