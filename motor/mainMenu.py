c6283edf7e952264dc577e17d1b46faf65608e0d
Stergios Matthaiakis <stergiosmat@gmail.com>
2022-12-29 17:28:06 +0200

    dfg

--- motor/mainMenu.py
+++ motor/mainMenu.py
@@ -14,7 +14,7 @@ class App(QApplication):
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
 
-        motor=MoveController(True);
+        motor=MoveController(False);
         
         # motor.rotate()
         #motor.complex2()
@@ -28,49 +28,46 @@ class App(QApplication):
         # motor.shouldersuphandsmove()
         
 
-        # motor.moveMotorsAbs(180,	433,818,196,818,572)
-        # time.sleep(2)
+        motor.moveMotorsAbs(180,	433,818,196,818,572)
+        time.sleep(2)
 
-        # motor.moveMotorsAbs(51	,229,	760,	239,	573,	714)
-        # time.sleep(2)
-        # motor.moveMotorsAbs(55,	449,	809,	131,	712,	882)
+        motor.moveMotorsAbs(51	,229,	760,	239,	573,	714)
+        time.sleep(2)
+        motor.moveMotorsAbs(55,	449,	809,	131,	712,	882)
         
-        # motor.moveMotors(20,	10,0,0,0,0)
-        # time.sleep(0.2)
-        # motor.moveMotors(-20,	-10,0,0,0,0)
-        # time.sleep(0.1)
-        # motor.moveMotors(20,	10,0,0,0,0)
-        # time.sleep(0.2)
-        # motor.moveMotors(-20,	-10,0,0,0,0)
+        motor.moveMotors(20,	10,0,0,0,0)
+        time.sleep(0.2)
+        motor.moveMotors(-20,	-10,0,0,0,0)
+        time.sleep(0.1)
+        motor.moveMotors(20,	10,0,0,0,0)
+        time.sleep(0.2)
+        motor.moveMotors(-20,	-10,0,0,0,0)
 
-        # time.sleep(0.2)
-        # motor.moveMotors(0,	0,0,0,0,30)
-        # time.sleep(0.2)
-        # motor.moveMotors(0,	0,0,0,0,-30)
+        time.sleep(0.2)
+        motor.moveMotors(0,	0,0,0,0,30)
+        time.sleep(0.2)
+        motor.moveMotors(0,	0,0,0,0,-30)
 
-        # time.sleep(0.2)
-        # motor.moveMotors(0,	0,0,0,0,50)
-        # time.sleep(0.2)
-        # motor.moveMotors(0,	0,0,0,0,-50)
+        time.sleep(0.2)
+        motor.moveMotors(0,	0,0,0,0,50)
+        time.sleep(0.2)
+        motor.moveMotors(0,	0,0,0,0,-50)
         
-        # time.sleep(0.2)
-        # motor.moveMotors(0,	0,0,0,50,0)
-        # time.sleep(0.2)
-        # motor.moveMotors(0,	0,0,0,-50,0)
+        time.sleep(0.2)
+        motor.moveMotors(0,	0,0,0,50,0)
+        time.sleep(0.2)
+        motor.moveMotors(0,	0,0,0,-50,0)
+    
+
+        time.sleep(0.2)
+        motor.moveMotors(25,	25,25,25,100,20)
+
+        time.sleep(0.3)
+        motor.moveMotors(-20,	-20,-20,-20,-20,-20)
+
+        time.sleep(1)
         
-
-
-
-        # time.sleep(0.2)
-        # motor.moveMotors(25,	25,25,25,100,20)
-
-
-        # time.sleep(0.3)
-        # motor.moveMotors(-20,	-20,-20,-20,-20,-20)
-
-        # time.sleep(1)
-        
-        # motor.moveMotorsAbs(180,	433,818,196,818,572)
+        motor.moveMotorsAbs(180,	433,818,196,818,572)
 
         self.menu_view =MenuView(motor)
         self.menu_view.show()
